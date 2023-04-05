9be3825e6e56b92109970992a40dd856caad8c05
thulerjoao <thuler_lima@hotmail.com>
2023-04-04 22:46:03 -0300

    feat: implemented methods to update(patch) and delete in table entity

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TableService } from './table.service';
 import { CreateTableDto } from './table.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Table } from './entities/table.entity';
+import { UpdateTableDto } from './update-table.dto';
 
 @ApiTags('table')
 @Controller('table')
@@ -13,7 +24,7 @@ export class TableController {
   @ApiOperation({
     summary: 'Buscar mesa por id',
   })
-  findOne(@Param(`id`) id:string): Promise<Table> {
+  findOne(@Param(`id`) id: string): Promise<Table> {
     return this.tableService.findOne(id);
   }
 
@@ -31,5 +42,22 @@ export class TableController {
   })
   create(@Body() dto: CreateTableDto) {
     return this.tableService.create(dto);
+  }
+
+  @Patch(`:id`)
+  @ApiOperation({
+    summary: 'Atualizar mesa por id',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateTableDto): Promise<Table> {
+    return this.tableService.update(id, dto);
+  }
+
+  @Delete(`:id`)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Excluir mesa por id',
+  })
+  delete(@Param(`id`) id: string){
+    this.tableService.delete(id)
   }
 }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -2,10 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { Table } from './entities/table.entity';
 import { CreateTableDto } from './table.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateTableDto } from './update-table.dto';
 
 @Injectable()
 export class TableService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async delete(id: string) {
+    await this.prisma.table.delete({ where: { id }})
+  }
+
+  update(id: string, dto: UpdateTableDto): Promise<Table> {
+    const data: Partial<Table> = { ...dto}
+
+    return this.prisma.table.update({
+      where: { id },
+      data
+    })
+  }
 
   findOne( id:string ): Promise<Table> {
     return this.prisma.table.findUnique({ where: { id } });
@@ -20,4 +34,5 @@ export class TableService {
 
     return this.prisma.table.create({ data }); //({data:data}) is the same ad ({data}) ==> implicit
   }
+
 }
