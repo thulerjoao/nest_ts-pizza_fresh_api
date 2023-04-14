ada20eece4b156368327c50ddbcd76e4ef052897
thulerjoao <thuler_lima@hotmail.com>
2023-04-13 21:55:04 -0300

    fix: product in create order fixed

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -20,8 +20,35 @@ export class OrderService {
           number: dto.tableNumber,
         },
       },
+      product: {
+        connect: dto.products.map((productId) => ({
+          id: productId,
+        })),
+      },
     };
-    return this.prisma.order.create({data}).catch(handleError)
+    return this.prisma.order
+      .create({
+        data,
+        select: {
+          id: true,
+          table: {
+            select: {
+              number: true,
+            },
+          },
+          user: {
+            select: {
+              name: true,
+            },
+          },
+          product: {
+            select: {
+              title: true,
+            },
+          },
+        },
+      })
+      .catch(handleError);
   }
 
   findAll() {
