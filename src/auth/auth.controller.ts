5c1cadf054519a3b7750a7c4ec782215661a1679
joao pedro thuler lima <thuler_lima@hotmail.com>
2023-04-17 18:26:53 -0300

    feat: implemented auth.service with login function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
+import { LoginResponseDto } from './dto/login-response.dto';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -9,5 +10,11 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  login(@Body() dto: LoginDto) {}
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Realizar login, recebendo um token de autenticação',
+  })
+  login(@Body() dto: LoginDto): Promise<LoginResponseDto> {
+    return this.authService.login(dto)
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { LoginResponseDto } from './dto/login-response.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  async login(dto: LoginDto): Promise<LoginResponseDto> {
+    return{
+      token: 'teste',
+      user: undefined
+    }
+  }
+}
