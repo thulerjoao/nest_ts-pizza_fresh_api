35447c4541d6808201f46ed2162956706d71a76a
joao pedro thuler lima <thuler_lima@hotmail.com>
2023-04-17 18:12:14 -0300

    feat:route auth created and login dto

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+@ApiTags('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  login(@Body() dto: LoginDto) {}
+}
