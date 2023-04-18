b392310454a41c4aac82e5d897c1862d83731582
joao pedro thuler lima <thuler_lima@hotmail.com>
2023-04-18 18:41:45 -0300

    feat: cors and https protocols opened

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  app.useStaticAssets(join(__dirname, '..', 'client'));
 
   const config = new DocumentBuilder()
     .setTitle('PizzaFresh')
